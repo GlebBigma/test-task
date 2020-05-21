5c6b3b97a1a5c4836acfb11cd20474b2bb744a77
Gleb Bigma <glebbigma@gmail.com>
2020-05-22 00:18:48 +0300

    Create gh-pages branch with static content

---
+++ precache-manifest.24b0aa108e9c071088059ace2f2290a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf48e0b04fd254784a389683e6f5730",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "8984564561c8f609ef7f",
+    "url": "/test-task/static/css/main.b1623cc1.chunk.css"
+  },
+  {
+    "revision": "18f8f43ed234186c71b1",
+    "url": "/test-task/static/js/2.0f0b6194.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-task/static/js/2.0f0b6194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8984564561c8f609ef7f",
+    "url": "/test-task/static/js/main.5cf76827.chunk.js"
+  },
+  {
+    "revision": "ca4d81169225afe2a98c",
+    "url": "/test-task/static/js/runtime-main.eb91428b.js"
+  },
+  {
+    "revision": "9ebeb518341fe3eaaa27c499f27fde21",
+    "url": "/test-task/static/media/img_pic.9ebeb518.svg"
+  },
+  {
+    "revision": "1876dfc74a758f2c09c0fbee6a53e5bf",
+    "url": "/test-task/static/media/logo.1876dfc7.svg"
+  }
+]);
